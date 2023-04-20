386e4f02c35ee1a97c965b16184e8304da5c09a8
Victor Hugo <victoralmeidadev.io@gmail.com>
2023-04-20 14:45:36 -0300

    version-2.0

--- script/script.js
+++ script/script.js
@@ -64,6 +64,21 @@ function createTable(data) {
     data.forEach((item) => {
         const div = document.createElement("div");
         div.classList.add("table-data");
+        
+        const classification = document.createElement("p");
+        classification.innerText = item.classification;
+    
+        const info = document.createElement("p");
+        info.innerText = item.info;
+    
+        const obesity = document.createElement("p");
+        obesity.innerText = item.obesity;
+    
+        div.appendChild(classification);
+        div.appendChild(info);
+        div.appendChild(obesity);
+    
+        imcTable.appendChild(div);
 
     });
 }

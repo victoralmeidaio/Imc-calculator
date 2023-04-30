6a4ab0dcd97c091301859977fdc6dea2b88d9bbc
victoralmeidaio <victoralmeidadev.io@gmail.com>
2023-04-30 17:32:42 -0300

    version2.0

--- script/script.js
+++ script/script.js
@@ -64,20 +64,20 @@ function createTable(data) {
     data.forEach((item) => {
         const div = document.createElement("div");
         div.classList.add("table-data");
-        
+
         const classification = document.createElement("p");
         classification.innerText = item.classification;
-    
+
         const info = document.createElement("p");
         info.innerText = item.info;
-    
+
         const obesity = document.createElement("p");
         obesity.innerText = item.obesity;
-    
+
         div.appendChild(classification);
         div.appendChild(info);
         div.appendChild(obesity);
-    
+
         imcTable.appendChild(div);
 
     });
@@ -88,7 +88,8 @@ function validDigits(text) {
 }
 
 function calcImc(height, weight) {
-    const imc = (weight / (height * height)).toFixed(1);
+    const heightInMetters = height / 100;
+    const imc = (weight / (heightInMetters ** 2)).toFixed(1);
     return imc;
 }
 
